8ba979b9d1ed478843068b3fc372e9bdfcc6276d
alvablot <89981142+alvablot@users.noreply.github.com>
2021-12-13 13:43:28 +0100

    Add files via upload

--- js/msc.js
+++ js/msc.js
@@ -1,32 +1,4 @@
-const sounds = [];
-sounds[0] = document.createElement("audio");
-sounds[0].src = "sounds/hyprbf1a.wav";
-sounds[1] = document.createElement("audio");
-sounds[1].src = "sounds/railgf1a.wav";
-sounds[2] = document.createElement("audio");
-sounds[2].src = "sounds/spacecombat.mp3";
-sounds[3] = document.createElement("audio");
-sounds[3].src = "sounds/explosion3.mp3";
-sounds[4] = document.createElement("audio");
-sounds[4].src = "sounds/scream2.mp3";
-sounds[5] = document.createElement("audio");
-sounds[5].src = "sounds/applause.mp3";
-sounds.forEach((sound) => {
-  sound.setAttribute("preload", "auto");
-  sound.setAttribute("controls", "none");
-  sound.style.display = "none";
-  document.body.appendChild(sound);
-  sound.addEventListener("canplaythrough", (event) =>{
-    console.log(`${sound.src} is loaded`)
-  })
-});
-function playSound(obj) {
-  if (sounds[obj].paused) {
-    sounds[obj].play();
-  } else {
-    sounds[obj].currentTime = 0;
-  }
-}
+
 // Variabler för olika vektorer mm som har med hastighet, placering etc att göra
 const startHastighet = 0.55;
 const skepp = [];
